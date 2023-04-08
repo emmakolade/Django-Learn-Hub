a6ec183f8afd452381970ecc16d05950999de806
Akolade <emmakolade@gmail.com>
2023-04-08 18:19:46 +0100

    learnhub

--- learnhub/getvideos/urls.py
+++ learnhub/getvideos/urls.py
@@ -2,7 +2,10 @@ from django.urls.conf import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
-    path('video_list/', views.get_videos, name='video_list'),
+    # path('', views.base, name='base'),
+    path('', views.get_videos, name='video_list'),
     path('download/<str:video_id>/', views.download_video, name='download'),
+    path('download_quality/<str:video_id>/',
+         views.download_quality, name='download_quality'),
+
 ]
--- learnhub/getvideos/views.py
+++ learnhub/getvideos/views.py
@@ -12,14 +12,12 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # Un-Authenticated Users will be redirected here
-
-
-def base(request):
-    return render(request, 'base/base.html')
+# def base(request):
+#     return render(request, 'base/base.html')
 
 
 # GET YOUTUBE VIDOES FROM YOUTUBE API
-@login_required
+
 def get_videos(request):
     query = request.GET.get('q', '')
     # keywords = ['Django', 'Django web development']
@@ -52,17 +50,38 @@ def get_videos(request):
     return render(request, 'getvideos/video_list.html', {'videos': videos})
 
 
-# PYTUBE TO DOWNLOAD VIDEOS
-@login_required
-def download_video(request, video_id):
+def download_quality(request, video_id):
+    url = f'https://www.youtube.com/watch?v={video_id}'
+    yt = YouTube(url)
+    stream = yt.streams.filter(progressive=True, file_extension='mp4')
+    video_qualities = [s.resolution for s in stream]
+    context = {
+        'video_id': video_id,
+        'video_title': yt.title,
+        'video_qualities': video_qualities
+    }
+    return render(request, 'getvideos/video_list.html', context)
+
+
+def download_video(request, video_id, quality):
     url = f'https://www.youtube.com/watch?v={video_id}'
     yt = YouTube(url)
     stream = yt.streams.filter(
-        progressive=True, file_extension='mp4').first()
+        progressive=True, file_extension='mp4', resolution=quality).first()
     video_file = stream.download()
     response = FileResponse(open(video_file, 'rb'))
-    response['Content-Disposition'] = f'attachment; filename="{yt.title}.mp4"'
+    response['Content-Disposition'] = f'attachment; filename="{yt.title} ({quality}).mp4"'
     return response
 
+
+# PYTUBE TO DOWNLOAD VIDEOS
+# def download_video(request, video_id):
+#     url = f'https://www.youtube.com/watch?v={video_id}'
+#     yt = YouTube(url)
+#     stream = yt.streams.filter(
+#         progressive=True, file_extension='mp4').first()
+#     video_file = stream.download()
+#     response = FileResponse(open(video_file, 'rb'))
+#     response['Content-Disposition'] = f'attachment; filename="{yt.title}.mp4"'
+#     return response
 # GET https: // youtube.googleapis.com/youtube/v3/search?part = snippet &maxResults = 25&q = surfing&key = [YOUR_API_KEY] HTTP/1.1
- 
